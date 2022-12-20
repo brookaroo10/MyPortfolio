5947db2014dbc5ec771c3144963168e65edd7e20
brookaroo10 <brookedev10@protonmail.com>
2022-12-19 20:29:19 -0500

    adding JS file that interacts with text

---
+++ MyPortfolio/script.js
@@ -0,0 +1,12 @@
+let BoldItems = document.querySelectorAll('.live_preview');
+
+
+function BoldText(){
+	BoldItems.style.fontweight = 'bold';
+}
+
+function UnBoldText(){
+	BoldItems.style.fontweight = 'normal';
+}
+
+
